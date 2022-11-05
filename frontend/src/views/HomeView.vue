0303fd2acf0ab2d1719e14a57701794940cd9244
Joel Juhela <joel.juhela@protonmail.com>
2022-11-06 00:41:56 +0200

    Fun animations

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -22,5 +22,22 @@ export default {
 <style>
     .centered {
         margin: 160px auto;
+        animation-iteration-count: infinite;
+        animation-name: updown;
+        animation-duration: 3s;
+        animation-timing-function: ease-in-out;
+    }
+
+    @keyframes updown {
+        0% {
+            transform: translateY(0);
+        }
+        50% {
+            transform: translateY(30px);
+        }
+        100% {
+            transform: translateY(0);
+        }
+
     }
 </style>
