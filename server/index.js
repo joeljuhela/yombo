951c2d61a6f14e12503664da67d6c7e17c5417a0
Joel Juhela <joel.juhela@bluugo.fi>
2022-11-05 15:10:42 +0200

    Add cors to backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = require('./app')
 const config = require('./config')
+const cors = require('cors')
 
 const port = config.port
+app.use(cors())
 app.listen(port, () => {
   console.log(`YOMBO server listening on port ${port}`)
 })
