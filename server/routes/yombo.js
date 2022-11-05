1699c0cda69b54f732560478b39911e09339e50f
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2022-11-05 18:41:43 +0200

    Get a random daily task

--- server/routes/yombo.js
+++ server/routes/yombo.js
@@ -1,9 +1,34 @@
 const router = require('express').Router()
+const { Op } = require('sequelize')
+const models = require('../models')
 
 // we can now trust `req.user`
 
-router.get('/dailyTasks', async (req, res) => {
-  res.json(await req.user.getTasks())
+router.get('/dailyTask', async (req, res) => {
+  const categories = [
+    'CONTACT',
+    'COLLABORATE',
+    'CONSIDER',
+  ]
+  let category = req.query.category
+  if (!category || !categories.includes(category)) {
+    res.status(400)
+    return res.json({
+      error: 'GET parameter `category` unavailable or incorrect'
+    })
+  }
+  const lessPointsThan = req.query.lessPointsThan || Number.MAX_VALUE
+  const tasks = await models.Task.findAll({
+    where: {
+      points: {
+        [Op.lt]: lessPointsThan,
+      },
+      category,
+    },
+  })
+  if (tasks.length === 0)
+    return res.json(null)
+  res.json(tasks[Math.floor(Math.random() * tasks.length)])
 })
 
 router.post('/createSubmission', async (req, res) => {
