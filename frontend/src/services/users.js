1855a54e9fd1b690090a6233a4eeba80b0d09c54
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2022-11-05 17:44:53 +0200

    Add `config.js`

--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -2,10 +2,9 @@ import axios from 'axios'
 import config from '../config.js'
                          
 const register = async creds => {
-  console.log(creds)
-  const baseUrl = `http://localhost:3000/api/v1/createYombo`
-  const response = await axios.post(baseUrl, creds)
-  return response.data
+    const baseUrl = `${config.apiBase}/api/v1/createYombo`
+    const response = await axios.post(baseUrl, creds)
+    return response.data
 }
 
 export default { register }
