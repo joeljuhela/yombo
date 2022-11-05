0d3b28b81ae6dbeb74ab4ec4029c4b100aac7db4
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2022-11-05 20:00:38 +0200

    Add great error handling
    Special thanks to black magic of `express-async-errors`

--- server/app.js
+++ server/app.js
@@ -1,15 +1,18 @@
 const express = require('express')
+require('express-async-errors')
 const cors = require('cors')
 const config = require('./config')
 const accounts = require('./routes/accounts')
 const yombo = require('./routes/yombo')
 const getUser = require('./middleware/getUser')
 const authOr403 = require('./middleware/authOr403')
+const errorHandler = require('./middleware/errorHandler')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(config.apiBase, accounts)
 app.use(`${config.apiBase}/:accessToken`, getUser, authOr403, yombo)
+app.use(errorHandler)
 
 module.exports = app
