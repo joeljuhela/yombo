867ec5864236e915145f0e522c7e64ffb02f16b4
Joel Juhela <joel.juhela@bluugo.fi>
2022-11-05 17:50:27 +0200

    Add homeview

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    This is the homeView
+    {{ $route.params.id }}
+    <div class="centered">
+        <yomboSVG/>
+    </div>
+  </div>
+</template>
+
+<script>
+import yomboSVG from '@/assets/svgs/yombo.vue'
+
+export default {
+    name: 'HomeView',
+    components: {
+        yomboSVG
+    }
+}
+</script>
+
+<style>
+    .centered {
+        margin: 160px auto;
+    }
+</style>
