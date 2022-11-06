4632bfdc498c241c15f85ffb7e09f0c8bdd5f4d6
Joel Juhela <joel.juhela@protonmail.com>
2022-11-06 03:12:45 +0200

    Change RegisterView to have only one root element

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,33 +1,35 @@
 <template>
-<h1>YOMBO</h1>
-<p>
-<i>Exercise Your Social Muscle</i>
-</p>
+  <div>
+    <h1>YOMBO</h1>
+    <p>
+    <i>Exercise Your Social Muscle</i>
+    </p>
 
-<h2>Create your own YOMBO</h2>
-<div>
-<p v-if="errors.length">
-<b>Please correct the following error(s):</b>
-<ul>
-  <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
-</ul>
-</p>
-<form @submit="register" method="post">
-  <input v-model="form.username" placeholder="username" type="text"/>
-  <input v-model="form.passwordInput1" placeholder="password" type="password">
-  <input v-model="form.passwordInput2" placeholder="password (again)" type="password">
-  <input type="submit" value="Register"/>
-</form>
-</div>
-<h2>Already have a YOMBO?</h2>
-<p>
-To keep your YOMBO safe, each of them is protected by both a password and a secret access token that's impossible to guess by outsiders.
-When you save your YOMBO to a bookmark, it's best to save your YOMBO's home address to a bookmark, so you can always easily find your way back.
-</p>
-<p>
-TIP: if you forgot to save your YOMBO to a bookmark, use your browser's history to find the correct address.
-And this time, be sure to bookmark!
-</p>
+    <h2>Create your own YOMBO</h2>
+    <div>
+    <p v-if="errors.length">
+    <b>Please correct the following error(s):</b>
+    <ul>
+      <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
+    </ul>
+    </p>
+    <form @submit="register" method="post">
+      <input v-model="form.username" placeholder="nickname" type="text"/>
+      <input v-model="form.passwordInput1" placeholder="password" type="password">
+      <input v-model="form.passwordInput2" placeholder="password (again)" type="password">
+      <input type="submit" value="Register"/>
+    </form>
+    </div>
+    <h2>Already have a YOMBO?</h2>
+    <p>
+    To keep your YOMBO safe, each of them is protected by both a password and a secret access token that's impossible to guess by outsiders.
+    When you save your YOMBO to a bookmark, it's best to save your YOMBO's home address to a bookmark, so you can always easily find your way back.
+    </p>
+    <p>
+    TIP: if you forgot to save your YOMBO to a bookmark, use your browser's history to find the correct address.
+    And this time, be sure to bookmark!
+    </p>
+  </div>
 </template>
 
 <script>
